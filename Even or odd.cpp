a0fbee8b8d6c64568845069cb2cc5a23da671940
rituja22 <109826066+rituja22@users.noreply.github.com>
2022-09-05 00:00:57 +0530

    Add files via upload

---
+++ Even or odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n ;
+	cout<<"Enter value \n";
+	cin>>n;
+	if(n%2 == 0)  
+    	cout<<"It's even";
+	else       
+		cout<<"It's odd";
+		
+	return 0;
+}
